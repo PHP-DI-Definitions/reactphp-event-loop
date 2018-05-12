9c5e455ba9a88e73bd590acfa0149d34b34cd5e1
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-05-12 18:16:22 +0200

    Made the definition simpler

--- src/event-loop.php
+++ src/event-loop.php
@@ -3,9 +3,8 @@
 use React\EventLoop\Factory;
 use React\EventLoop\LoopInterface;
 
-return new class {
-    public function __invoke(): LoopInterface
-    {
+return [
+    LoopInterface::class => function () {
         return Factory::create();
-    }
-};
+    },
+];
