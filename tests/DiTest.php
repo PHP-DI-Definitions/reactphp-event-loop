eff851bbcf305788db43591792a1f99a1645aa27
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-11-23 07:48:42 +0100

    Updated to new CS rules

--- tests/DiTest.php
+++ tests/DiTest.php
@@ -3,11 +3,14 @@
 use ApiClients\Tools\TestUtilities\TestCase;
 use React\EventLoop\LoopInterface;
 
+/**
+ * @internal
+ */
 final class DiTest extends TestCase
 {
-    public function testLoop()
+    public function testLoop(): void
     {
-        $root = dirname(__DIR__);
+        $root = \dirname(__DIR__);
         $path = $root . DIRECTORY_SEPARATOR . 'etc' . DIRECTORY_SEPARATOR . 'di' . DIRECTORY_SEPARATOR . 'event-loop.php';
         $loop = (require $path)[LoopInterface::class]();
 
